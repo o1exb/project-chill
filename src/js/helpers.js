2c4f7750492cfa7d392160aad8ac64907771f517
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-06-13 21:08:31 +0300

    hero

--- src/js/helpers.js
+++
@@ -1,9 +0,0 @@
-import { refs } from './refs.js';
-
-export function showLoader() {
-  refs.loader.classList.add('loader');
-}
-
-export function hideLoader() {
-  refs.loader.classList.remove('loader');
-}
--- src/js/mobile-menu.js
+++
@@ -1,5 +0,0 @@
-import { refs } from './refs.js';
-import { onBtnOpenMenuClick, onBtnCloseMenuClick } from './handlers';
-
-refs.burgerBtnOpenMenu.addEventListener('click', onBtnOpenMenuClick);
-refs.burgerBtnCloseMenu.addEventListener('click', onBtnCloseMenuClick);
